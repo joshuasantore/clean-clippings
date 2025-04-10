cb74390b3bbf56720ed0b016eedbb1f3da7d61ec
joshuasantore <santore.joshua@gmail.com>
2025-04-10 14:13:06 -0400

    Command line setup, accepts -d flag to specify whether given path is to directory and path as argument

--- src/clean_clippings/cli/commands.py
+++ src/clean_clippings/cli/commands.py
@@ -2,7 +2,9 @@ import click
 
 @click.command()
 @click.option('-d', '--dir', is_flag=True, help='path is to a directory')
-@click.argument('path', nargs=-1)
+@click.argument('path', nargs=-1, required=True)
 def cli(dir: bool, path):
 	print(path)
+	
+	
 
