1f06de18cd02296b24b5bc488d4d1de9bd898c66
joshuasantore <santore.joshua@gmail.com>
2025-04-12 12:58:39 -0400

    updated locate functions to avoid raising exceptions themselves

--- src/clean_clippings/file/locate.py
+++ src/clean_clippings/file/locate.py
@@ -2,8 +2,11 @@ import os
 
 def locateDirectory(path):
 	if not os.path.isdir(path):
-		raise NotADirectoryError(f"No directory found matching: '{path}'")
+		return False
+	return True
+		
 
 def locateFile(path):
 	if not os.path.isfile(path):
-		raise FileNotFoundError(f"No file found matching: '{path}' ")
+		return False
+	return True
